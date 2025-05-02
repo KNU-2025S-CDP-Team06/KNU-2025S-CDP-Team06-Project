9010c28efc1f8ac3242638129b5677912c0c719c
MOJAN <sjs732@naver.com>
2025-05-02 22:41:00 +0900

    Feat: MLOps 추가

--- clone.sh
+++ clone.sh
@@ -16,3 +16,4 @@ pull_or_clone() {
 
 pull_or_clone https://github.com/KNU-2025S-CDP-Team06/KNU-2025S-CDP-Team06-Frontend.git Frontend
 pull_or_clone https://github.com/KNU-2025S-CDP-Team06/KNU-2025S-CDP-Team06-Backend.git Backend
+pull_or_clone https://github.com/KNU-2025S-CDP-Team06/KNU-2025S-CDP-Team06-MLOps.git MLOps
